7e2693485ad9d66c809da119eabf87b416021aa8
Evan <eosul001@odu.edu>
2019-03-20 18:40:33 -0400

    added admin main page

---
+++ app/src/main/java/com/example/topspin/admin_main_page.java
@@ -0,0 +1,13 @@
+package com.example.topspin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class admin_main_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main_page);
+    }
+}
