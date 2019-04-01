ec7e8f9d8415f3df29288ec3c44b5f6589c9c16b
cmurdock <cmurdock@cs.odu.edu>
2019-04-01 00:31:59 -0400

    Added edit player scores page

---
+++ www/topspin/edit.php
@@ -0,0 +1,36 @@
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#aabcfe;margin:0px auto;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:12px 20px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#669;background-color:#e8edff;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:12px 20px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aabcfe;color:#039;background-color:#b9c9fe;}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}</style>
+<div class="tg-wrap"><table class="tg">
+  <tr>
+    <th class="tg-0lax"></th>
+    <th class="tg-0lax">Match 1</th>
+    <th class="tg-0lax">Match 2</th>
+    <th class="tg-0lax">Match 3</th>
+    <th class="tg-0lax">Win</th>
+  </tr>
+  <tr>
+    <td class="tg-0lax">Player 1</td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">Player 2</td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">Player 3</td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+    <td class="tg-0lax"><input type="text" name="" value=""></td>
+  </tr>
+</table></div>
