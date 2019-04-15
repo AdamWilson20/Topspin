dab9b585c980671fe8112e276f1ba366ff5613e0
bsimpson <bsimp002@odu.edu>
2019-04-15 17:29:31 -0400

    DB URL correction

--- app/src/main/java/com/example/topspin/Constants.java
+++ app/src/main/java/com/example/topspin/Constants.java
@@ -2,7 +2,7 @@ package com.example.topspin;
 
 public class Constants {
 
-    private static final String ROOT_URL = "https://411reds19.cs.odu.edu/Android/v1/";
+    private static final String ROOT_URL = "https://411reds19.cs.odu.edu/tsproto/Android/v1/";
 
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
 
