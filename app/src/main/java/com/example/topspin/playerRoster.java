77b24522269e96be6cedc72c3e40daa733a5e516
bsimpson <bsimp002@odu.edu>
2019-04-23 00:24:42 -0400

    small changes

--- app/src/main/java/com/example/topspin/playerRoster.java
+++ app/src/main/java/com/example/topspin/playerRoster.java
@@ -84,63 +84,7 @@ public class playerRoster extends AppCompatActivity {
     private void populateRoster() {
         roster = new RosterAdapter(this, players);
         listView.setAdapter(roster);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                int tempID = players.get(position).getPlayerID();
-                Toast.makeText(getApplicationContext(), String.valueOf(tempID), Toast.LENGTH_SHORT).show();
-                Intent startViewModify;
-                startViewModify = new Intent(getApplicationContext(), ViewScheduleEntry.class);
-                startViewModify.putExtra("ID",tempID);
-                startActivity(startViewModify);
-
-            }
-        });
     }
-
-
-    //Create Roster objects
-
-        /*
-        //Add the Player objects to an ArrayList
-        ArrayList<Player> rosterList = new ArrayList<>();
-        for(int i = 0 ;i < 20; i++){
-            Player adam = new Player("Adam","Senior","Virginia","5'8","160");
-            rosterList.add(adam);
-
-        Player one = new Player("Jannik Giesse", "Sophomore", "Ketsch,Germany", "6'1", "160");
-        Player two = new Player("Justin Herrera", "Junior", "Miami, Florida", "5'9", "160");
-        Player three = new Player("Aziz Kijametovic", "Senior", "Zivinice, Bosinia", "6'0", "160");
-        Player four = new Player("Luca Maldoner", "Freshman", "Innsbruck, Austria", "6'1", "160");
-        Player five = new Player("Bradley Mittleman", "Freshman", "Leawood, Kansas", "6'0", "160");
-        Player six = new Player("Francois Musitelli", "Junior", "Dijon, France", "6'0", "160");
-
-        //Add the Player objects to an ArrayList
-        //ArrayList<Player> rosterList = new ArrayList<>();
-        rosterList.add(one);
-        rosterList.add(two);
-        rosterList.add(three);
-        rosterList.add(four);
-        rosterList.add(five);
-        rosterList.add(six);
-
-
-        }
-        PlayerListAdapater adapter = new PlayerListAdapater(this, R.layout.adapter_player, rosterList);
-        mListView.setAdapter(adapter);
-
-
-    }
-
-
-    private void openActivity() {
-        Intent intent = new Intent(this, AddPlayerEntry.class);
-        startActivity(intent);
-    }
-  */
-
 
     public void getRoster() {
         StringRequest stringRequest = new StringRequest(Request.Method.POST,
